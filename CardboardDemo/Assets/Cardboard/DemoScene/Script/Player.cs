e6cfff1aee944e684ef4fbdf2ace873debf85d35
Sandra <santr006@student.liu.se>
2016-03-21 22:19:36 +0100

    demo app + collision and walking

---
+++ CardboardDemo/Assets/Cardboard/DemoScene/Script/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    private Cardboard cardboard;
+    private GameObject camera;
+    private Rigidbody rb;
+
+    private bool walking = false;
+    private bool collided = false;
+
+
+
+    private Vector3 lastPos;
+
+	// Use this for initialization
+	void Start () {
+        cardboard = GetComponent<Cardboard>();
+        camera = transform.GetChild(0).GetChild(0).gameObject;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        //walk
+        if (walking)
+        {
+            Debug.Log("walking");
+            rb.velocity = camera.transform.forward * speed;            
+        }
+        else
+        {
+            rb.velocity = new Vector3(0,0,0); 
+        }
+
+        //start/stop walking
+        if (cardboard.Triggered)
+        {
+            if (walking)
+            {
+                walking = false;
+            }
+            else
+            {
+                walking = true;
+            }            
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+        Debug.Log("collision");
+    }
+}
---
+++ CardboardDemo/Assets/Cardboard/DemoScene/Script/Spirit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spirit : MonoBehaviour {
+
+	public float radius = 3.0f;
+    public float speed = 1.0f;
+    public float height = 1.0f;
+
+	// Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(0, height, radius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Cos(Time.time * speed) * radius, height, Mathf.Sin(Time.time * speed) * radius);
+	}
+}
